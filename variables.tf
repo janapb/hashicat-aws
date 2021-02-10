010e2834edaead5d04e7d5174296d55e1951e3bb
janapb <janakiraman22@example.com>
2021-02-10 19:07:12 +0000

    Added s3 bucket module

--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
+  default = "bucket_prefix"
 }
 
 variable "region" {
@@ -48,3 +49,4 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
